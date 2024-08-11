1f29bc52abdeada8bbed60a84a9a82144e3541d5
sangeeth-Chejerla <sangeethreddychejerla@gmail.com>
2024-08-11 12:46:07 +0530

    added explaination to 344

--- leetcode/344_RevereString.py
+++ leetcode/344_RevereString.py
@@ -1,3 +1,9 @@
+"""
+Reversing the List: The slice b[::-1] creates a new list that is the reverse of b. 
+The [:] notation is used to assign this reversed list back to b, effectively modifying b in place.
+"""
+
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
 
